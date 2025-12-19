033c0a8507d2e19931db927803d5ce77cc50d1a1
KittenAmogus <kittenamogus3@gmail.com>
2025-12-19 12:33:13 +0000

    Empty frame

--- src/com/kittenamogus/game/Game.java
+++ src/com/kittenamogus/game/Game.java
@@ -3,10 +3,16 @@ package com.kittenamogus.game;
 import com.kittenamogus.graphics.Graphics;
 
 public class Game {
+	static int ROW	=	8;
+	static int COL	=	8;
+	static int MINES	=	10;
+
+	static String TITLE		=	"JavaSweeper";
+
 	Graphics graphics;
 
 	public Game() {
-		this.graphics = new Graphics();
+		this.graphics = new Graphics( COL, ROW, TITLE );
 	}
 
 	// Init
@@ -16,7 +22,8 @@ public class Game {
 	// Start
 
 	public void start() {
-		graphics.drawFull();
+		this.graphics.init();
+		this.graphics.drawFull();
 	}
 }
 
--- src/com/kittenamogus/graphics/Graphics.java
+++ src/com/kittenamogus/graphics/Graphics.java
@@ -1,8 +1,37 @@
 package com.kittenamogus.graphics;
 
+import javax.swing.*;
+
 
 public class Graphics {
-	public Graphics() {
+	int COL;
+	int ROW;
+	String TITLE;
+
+	JFrame frame;
+
+	public Graphics( int col, int row, String title ) {
+		this.COL	=	col;
+		this.ROW	=	row;
+		this.TITLE	=	title;
+
+		this.frame	=	new JFrame( TITLE );
+	}
+
+	// Private
+
+	private void configureFrame() {
+		this.frame.setResizable( false );
+		this.frame.setSize( 800, 800 );
+		this.frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+		this.frame.setVisible( true );
+	}
+
+	// Public
+
+	public void init() {
+		this.configureFrame();
 	}
 
 	public void draw() {
